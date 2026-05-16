377e0da0b714821854bb3793a7c04bcdf8cc660d
Atson Melo <104263082+AtsonMelo@users.noreply.github.com>
2026-05-16 19:52:02 -0300

    feat: adiciona selecao de tema no app

--- app/TestadorCLPHI.App/MainForm.cs
+++ app/TestadorCLPHI.App/MainForm.cs
@@ -1,3 +1,5 @@
+using Microsoft.Win32;
+
 namespace TestadorCLPHI.App;
 
 public sealed class MainForm : Form
@@ -5,6 +7,11 @@ public sealed class MainForm : Form
     private readonly Label _tituloLabel;
     private readonly Label _statusLabel;
     private readonly Button _pararTudoButton;
+
+    private readonly GroupBox _temaGroupBox;
+    private readonly RadioButton _usarTemaWindowsRadioButton;
+    private readonly RadioButton _temaClaroRadioButton;
+    private readonly RadioButton _temaEscuroRadioButton;
 
     public MainForm()
     {
@@ -40,11 +47,56 @@ public sealed class MainForm : Form
             Height = 40
         };
 
+        _temaGroupBox = new GroupBox
+        {
+            Text = "Tema",
+            Left = 20,
+            Top = 190,
+            Width = 260,
+            Height = 120
+        };
+
+        _usarTemaWindowsRadioButton = new RadioButton
+        {
+            Text = "Usar tema do Windows",
+            AutoSize = true,
+            Left = 15,
+            Top = 25
+        };
+
+        _temaClaroRadioButton = new RadioButton
+        {
+            Text = "Claro",
+            AutoSize = true,
+            Left = 15,
+            Top = 55
+        };
+
+        _temaEscuroRadioButton = new RadioButton
+        {
+            Text = "Escuro",
+            AutoSize = true,
+            Left = 15,
+            Top = 85
+        };
+
         _pararTudoButton.Click += PararTudoButton_Click;
+
+        _usarTemaWindowsRadioButton.CheckedChanged += TemaRadioButton_CheckedChanged;
+        _temaClaroRadioButton.CheckedChanged += TemaRadioButton_CheckedChanged;
+        _temaEscuroRadioButton.CheckedChanged += TemaRadioButton_CheckedChanged;
+
+        _temaGroupBox.Controls.Add(_usarTemaWindowsRadioButton);
+        _temaGroupBox.Controls.Add(_temaClaroRadioButton);
+        _temaGroupBox.Controls.Add(_temaEscuroRadioButton);
 
         Controls.Add(_tituloLabel);
         Controls.Add(_statusLabel);
         Controls.Add(_pararTudoButton);
+        Controls.Add(_temaGroupBox);
+
+        _usarTemaWindowsRadioButton.Checked = true;
+        AplicarTemaSelecionado();
     }
 
     private void PararTudoButton_Click(object? sender, EventArgs e)
@@ -54,5 +106,84 @@ public sealed class MainForm : Form
             "Parar tudo",
             MessageBoxButtons.OK,
             MessageBoxIcon.Information);
+    }
+
+    private void TemaRadioButton_CheckedChanged(object? sender, EventArgs e)
+    {
+        AplicarTemaSelecionado();
+    }
+
+    private void AplicarTemaSelecionado()
+    {
+        bool temaEscuro;
+
+        if (_usarTemaWindowsRadioButton.Checked)
+        {
+            temaEscuro = WindowsEstaEmTemaEscuro();
+        }
+        else if (_temaEscuroRadioButton.Checked)
+        {
+            temaEscuro = true;
+        }
+        else
+        {
+            temaEscuro = false;
+        }
+
+        AplicarTema(temaEscuro);
+    }
+
+    private void AplicarTema(bool temaEscuro)
+    {
+        Color corFundo;
+        Color corTexto;
+        Color corBotao;
+        Color corTextoBotao;
+
+        if (temaEscuro)
+        {
+            corFundo = Color.FromArgb(32, 32, 32);
+            corTexto = Color.WhiteSmoke;
+            corBotao = Color.FromArgb(55, 55, 55);
+            corTextoBotao = Color.White;
+        }
+        else
+        {
+            corFundo = SystemColors.Control;
+            corTexto = Color.Black;
+            corBotao = SystemColors.Control;
+            corTextoBotao = Color.Black;
+        }
+
+        BackColor = corFundo;
+
+        _tituloLabel.ForeColor = corTexto;
+        _statusLabel.ForeColor = corTexto;
+
+        _temaGroupBox.ForeColor = corTexto;
+        _temaGroupBox.BackColor = corFundo;
+
+        _usarTemaWindowsRadioButton.ForeColor = corTexto;
+        _usarTemaWindowsRadioButton.BackColor = corFundo;
+
+        _temaClaroRadioButton.ForeColor = corTexto;
+        _temaClaroRadioButton.BackColor = corFundo;
+
+        _temaEscuroRadioButton.ForeColor = corTexto;
+        _temaEscuroRadioButton.BackColor = corFundo;
+
+        _pararTudoButton.BackColor = corBotao;
+        _pararTudoButton.ForeColor = corTextoBotao;
+        _pararTudoButton.FlatStyle = FlatStyle.Flat;
+    }
+
+    private static bool WindowsEstaEmTemaEscuro()
+    {
+        using RegistryKey? key = Registry.CurrentUser.OpenSubKey(
+            @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+
+        object? value = key?.GetValue("AppsUseLightTheme");
+
+        return value is int appsUseLightTheme && appsUseLightTheme == 0;
     }
 }
